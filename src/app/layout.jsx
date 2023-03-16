a4f503c8515b3fcf2b00845e18861d28432bd000
Anthony Bigotte <bigotte.anthony@gmail.com>
2023-03-16 09:55:46 +0100

    installed tailwind css

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,9 @@ import './globals.css';
 export const metadata = {
   title: 'Google Clone Next js 13',
   description: 'Google clone created with Next js 13',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({ children }) {
