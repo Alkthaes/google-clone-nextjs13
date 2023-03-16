fccec5feb09196eae6822a7a2215ea9f04489fca
Anthony Bigotte <bigotte.anthony@gmail.com>
2023-03-16 09:35:36 +0100

    installed next js and created the first template

---
+++ src/app/layout.jsx
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Google Clone Next js 13',
+  description: 'Google clone created with Next js 13',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body>{children}</body>
+    </html>
+  );
+}
