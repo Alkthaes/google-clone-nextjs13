276dd78637875226ee10897347daef9383e503ac
Anthony Bigotte <bigotte.anthony@gmail.com>
2023-03-17 13:07:13 +0100

    fetch data from google search api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { RiH1 } from 'react-icons/ri';
 
-export default function WebSearchPage() {
-  return <div>WebSearchPage</div>;
+export default async function WebSearchPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+
+  const data = await response.json();
+  const results = data.items;
+
+  return (
+    <>
+      {results &&
+        results.map((result) => <h1 key={result.id}>{result.title}</h1>)}
+    </>
+  );
 }
